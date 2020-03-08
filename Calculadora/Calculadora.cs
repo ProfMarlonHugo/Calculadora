fae6dccb1e5864bb4bcae804d877923de5a6823d
Marlon <hugorodrigues6@gmail.com>
2020-03-07 21:20:03 -0300

    Meu primeiro commit

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgCalculadora
+{
+    class Calculadora
+    {
+        private List<double> numeros = new List<double>();
+
+        public void addItem(double item)
+        {
+            this.numeros.Add(item);
+        }
+
+        public double somar()
+        {
+            double resultado = 0;
+            foreach(double numero in this.numeros)
+            {
+                resultado += numero;
+            }
+            this.numeros.Clear();
+            return resultado;
+        }
+
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgCalculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+            calc.addItem(11.1);
+            calc.addItem(7);
+
+            System.Console.WriteLine(calc.somar());
+
+            System.Console.ReadKey();
+        }
+    }
+}
